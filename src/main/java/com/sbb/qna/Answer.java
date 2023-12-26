ff29230de887362321350f39e875f08b207a1118
본ì5binn <dhxnejqls13@naver.com>
2023-12-26 17:29:52 +0900

    qna2

---
+++ src/main/java/com/sbb/qna/Answer.java
@@ -0,0 +1,24 @@
+package com.sbb.qna;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+    @Id //PK 자동추가
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT") //TEXT 타입으로 생성
+    private String content;
+
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/com/sbb/qna/Question.java
@@ -0,0 +1,27 @@
+package com.sbb.qna;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id //PK 자동추가
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    private Integer id;
+    @Column(length = 200)
+    private String subject;
+    @Column(columnDefinition = "TEXT") //TEXT 타입으로 생성
+    private String content;
+    private LocalDateTime createdDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+
+}
